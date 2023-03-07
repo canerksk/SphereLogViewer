d75764c687fd92ce0df68028a74e0079d93dc113
Caner <gralrummy@gmail.com>
2023-03-07 20:25:25 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace SphereLogViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonLogFilesRefresh = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Select = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Dosya = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeight = 40;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView1.Location = new System.Drawing.Point(205, 38);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(914, 601);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // buttonLogFilesRefresh
+            // 
+            this.buttonLogFilesRefresh.Location = new System.Drawing.Point(12, 590);
+            this.buttonLogFilesRefresh.Name = "buttonLogFilesRefresh";
+            this.buttonLogFilesRefresh.Size = new System.Drawing.Size(184, 23);
+            this.buttonLogFilesRefresh.TabIndex = 2;
+            this.buttonLogFilesRefresh.Text = "Yenile";
+            this.buttonLogFilesRefresh.UseVisualStyleBackColor = true;
+            this.buttonLogFilesRefresh.Click += new System.EventHandler(this.buttonLogFilesRefresh_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(248, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(871, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Anahtar kelime giriniz.";
+            this.textBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseClick);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(202, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Arama:";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.linkLabel1.LinkVisited = true;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 616);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(184, 23);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://arcaneuo.com";
+            this.linkLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Select,
+            this.Dosya});
+            this.dataGridView2.Location = new System.Drawing.Point(5, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.Size = new System.Drawing.Size(194, 572);
+            this.dataGridView2.TabIndex = 6;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // Select
+            // 
+            this.Select.HeaderText = "#";
+            this.Select.Name = "Select";
+            this.Select.Width = 35;
+            // 
+            // Dosya
+            // 
+            this.Dosya.HeaderText = "Dosya";
+            this.Dosya.Name = "Dosya";
+            this.Dosya.ReadOnly = true;
+            this.Dosya.Width = 145;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1131, 647);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonLogFilesRefresh);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(1147, 686);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SphereServer Log Viewer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonLogFilesRefresh;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Select;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Dosya;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SphereLogViewer
+{
+    public partial class Form1 : Form
+    {
+        DataTable table;
+
+        public static string baselogspath = Path.Combine(Application.StartupPath, "logs");
+        public static int contentheight = 840;
+        public static int saatheight = 55;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            table = new DataTable();
+            table.Columns.Add("Saat", typeof(string));
+            table.Columns.Add("Content", typeof(string));
+
+        }
+
+        private bool IsBlank(string inString)
+        {
+            if (!string.IsNullOrEmpty(inString)) inString = inString.Trim();
+            return string.IsNullOrEmpty(inString);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(baselogspath))
+            {
+                MessageBox.Show("Logs klasörü bulunamadı!" + baselogspath);
+                Close();
+                return;
+            }
+
+            getAllLogFiles();
+        }
+
+        public void getLogContent(string filename)
+        {
+            string logfile = filename;
+
+            if (!File.Exists(filename))
+            {
+                MessageBox.Show("Log dosyası bulunamadı!");
+                return;
+            }
+
+
+            using (var streamReader = new StreamReader(filename))
+            {
+                while (!streamReader.EndOfStream)
+                {
+                    var line = streamReader.ReadLine();
+                    if (!IsBlank(line))
+                    {
+                        var values = line.Split('\t');
+                        int len = 0;
+                        string value = string.Empty;
+                        string content = string.Empty;
+
+                        for (int i = 0; i < values.Length; i++)
+                        {
+                            content = values[i];
+
+                            len = content.Length;
+                            if (content.Length > 0)
+                                value = content.Substring(6);
+
+                            string saat = content.Substring(0, 5);
+
+                            if (!content.Contains("Indexing the client files...") && value != "WARNING:" && !content.Contains("contain errors, might be unstable or even destroy your shard as they are mostly untested!") && !content.Contains("Nightly builds are automatically made at every commit to the source code repository and might") && !content.Contains("DO NOT run this build on a live shard unless you know what you are doing!") && !content.Contains("-----------------------------------------------------------------") && !content.Contains("This is a nightly build of SphereServer.") && !string.IsNullOrEmpty(value))
+                                table.Rows.Add(saat, value);
+                        }
+                    }
+
+                }
+
+            }
+
+            dataGridView1.DataSource = table;
+            dataGridView1.Columns[1].Width = contentheight;
+            dataGridView1.Columns[0].Width = saatheight;
+
+        }
+
+
+        public void getAllLogFiles()
+        {
+            //checkedListBox1.Items.Clear();
+            dataGridView2.Rows.Clear();
+
+            DirectoryInfo d = new DirectoryInfo(baselogspath);
+            FileInfo[] Files = d.GetFiles("*.log");
+            foreach (FileInfo file in Files)
+            {
+                dataGridView2.Rows.Add(false, file.Name);
+                //checkedListBox1.Items.Add(file.Name);
+            }
+
+
+
+        }
+
+        private void buttonLogFilesRefresh_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.Rows.Clear();
+            dataGridView1.Refresh();
+            dataGridView1.Columns[1].Width = contentheight;
+            dataGridView1.Columns[0].Width = saatheight;
+
+            getAllLogFiles();
+        }
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            DataView dv = table.DefaultView;
+            dv.RowFilter = "Content LIKE '%" + textBox1.Text + "%'";
+            dataGridView1.DataSource = dv;
+
+            dataGridView1.Columns[1].Width = contentheight;
+            dataGridView1.Columns[0].Width = saatheight;
+        }
+
+        private void textBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (textBox1.Text == "Anahtar kelime giriniz.")
+            {
+                textBox1.Text = "";
+            }
+            dataGridView1.Columns[1].Width = contentheight;
+            dataGridView1.Columns[0].Width = saatheight;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://arcaneuo.com");
+
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int rowindex = dataGridView2.CurrentCell.RowIndex;
+            int columnindex = dataGridView2.CurrentCell.ColumnIndex;
+
+            //if (dataGridView2.SelectedRows.Count <= 0)
+            //{
+                dataGridView1.DataSource = null;
+                dataGridView1.Rows.Clear();
+                dataGridView1.Refresh();
+            //}
+            //else
+            //{
+            //    dataGridView1.Refresh();
+            //    dataGridView1.DataSource = table;
+            //    dataGridView1.Columns[1].Width = contentheight;
+            //    dataGridView1.Columns[0].Width = saatheight;
+            //}
+
+            //if (e.ColumnIndex == Select.Index && e.RowIndex != -1)
+            if (columnindex == 0)
+            {
+                textBox1.Text = "Anahtar kelime giriniz.";
+
+                string filedir = "";
+                for (int i = 0; i < dataGridView2.SelectedCells.Count; i++)
+                {
+                    filedir = Path.Combine(baselogspath, dataGridView2.Rows[rowindex].Cells[1].Value.ToString());
+                    getLogContent(filedir);
+
+                    //Console.WriteLine(filedir);
+                }
+                //MessageBox.Show(filedir);
+
+            }
+        }
+
+
+    }
+}
